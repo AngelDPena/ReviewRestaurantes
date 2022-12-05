72feae6ffb8ca4dc36ba7434e99d98372706e62e
Angel_Pena <1103314@est.intec.edu.do>
2022-12-05 17:12:31 -0400

    Agregando Registro Restaurantes

--- main.py
+++ main.py
@@ -7,21 +7,15 @@ import maskpass as mp
 import os
 
 client = MongoClient(
-    'mongodb+srv://Angel:ycMVTPw6PNkHm1iy@cluster0.oqicaw7.mongodb.net/test')
+    'mongodb+srv://Angel:ycMVTPw6PNkHm1iy@cluster0.oqicaw7.mongodb.net/test')  # Connection string
 
-db = client.RestaurantReviews
-users = db.Users
-'''post = {
-    "Username": "juan",
-    "Password": "1234",
-    "Cedula":   "00111158963",
-    "Nombres": "Juan Augusto",
-    "Apellidos": "Perez Jimenez",
-    "Correo": "japj@gmail.com"
-}'''
-#users._insert_one(post, False, False, 1, False, None, "Insertando Usuario")
+db = client.RestaurantReviews  # DB Connection path
+
+users = db.Users  # Collection Users
+restaurant = db.Restaurant  # Collection Restaurant
+
 root = tk.Tk()
-root.geometry('400x150')
+root.geometry('200x100')
 root.title('Login')
 
 
@@ -46,15 +40,16 @@ def login(usr, pw):
 def ui():
 
     # username label and text entry box
-    usernameLabel = Label(root, text="User Name").grid(row=0, column=0)
+    spacer = Label(root, text=" ").grid(row=0, column=0)
+    usernameLabel = Label(root, text="User Name").grid(row=1, column=0)
     username = StringVar()
-    usernameEntry = Entry(root, textvariable=username).grid(row=0, column=1)
+    usernameEntry = Entry(root, textvariable=username).grid(row=1, column=1)
 
     # password label and password entry box
-    passwordLabel = Label(root, text="Password").grid(row=1, column=0)
+    passwordLabel = Label(root, text="Password").grid(row=2, column=0)
     password = StringVar()
     passwordEntry = Entry(root, textvariable=password,
-                          show='*').grid(row=1, column=1)
+                          show='*').grid(row=2, column=1)
 
     # login button
     loginButton = Button(root, text="Login", command=lambda: validateLogin(
@@ -119,7 +114,30 @@ def RegUser():
 
 
 def RegRestaurant():
-    print("Registro")
+    salir = 0
+    while salir != 1:
+        Nombre = input("Nombre del restaurante: ")
+        Direccion = input("Dirección: ")
+        ID = int(input("ID del Propietario: "))
+        Rating = float(input("Calificación: "))
+        print(" ")
+        post = {
+            "Name": Nombre,
+            "Address": Direccion,
+            "OwnerID": ID,
+            "Rating": Rating
+        }
+        if restaurant.find_one({'Name': Nombre} and {"Address": Direccion} and {"OwnerID": ID}) == None:
+            try:
+                restaurant.insert_one(post, False, None, "Insertando Usuario")
+                print("Restaurante agregado Exitosamente!")
+            except:
+                print(Exception)
+        else:
+            print("¡Restaurante existente!")
+        salir = int(input("Desea salir? 1 para salir 0 para quedarse: "))
+        os.system("cls")
+    return 0
 
 
 def Review():
